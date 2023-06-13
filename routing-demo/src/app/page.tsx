38da6ca09c8619429fd8d8ce5678dc3474d2556b
Vishwas <vishwas.g5@gmail.com>
2023-06-13 15:06:26 +0530

    Link component

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome home!</h1>;
+  return (
+    <div>
+      <h1>Welcome home!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </div>
+  );
 }
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,21 @@
-export default function ProductList() {
+import Link from "next/link";
+export default function ProductList({ productId = 100 }) {
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="products/3">Product 3</Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
   );
 }
