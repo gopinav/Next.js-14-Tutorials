e3078046041e95d61204e9383063ab4698cf6f53
Vishwas <vishwas.g5@gmail.com>
2023-06-20 15:30:22 +0530

    Prefetching

--- routing-demo/src/app/order-product/page.tsx
+++ routing-demo/src/app/order-product/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function OrderProduct() {
   const router = useRouter();
   const handleClick = () => {
     console.log("Placing your order");
-    router.push("/");
+    router.push("/blog");
   };
+
+  useEffect(() => {
+    router.prefetch("/blog");
+  }, [router]);
   return (
     <>
       <h1>Order product</h1>
