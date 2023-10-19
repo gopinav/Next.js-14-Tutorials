8f4451be3f44a5feb2a8fa1db9872eb55c18f85b
Vishwas <vishwas.g5@gmail.com>
2023-10-19 18:07:53 +0530

    Dynamic metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,17 +1,20 @@
-import { Metadata, ResolvingMetadata } from "next";
+import { Metadata } from "next";
 
 type Props = {
   params: { productId: string };
 };
 
-export const generateMetadata = async (
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> => {
-  const parentMetadata = await parent;
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = params;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productId);
+    }, 100);
+  });
   return {
-    title: `Product ${params.productId}`,
-    description: parentMetadata.title?.absolute,
+    title: `Product ${title}`,
   };
 };
 
