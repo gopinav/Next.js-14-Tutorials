d09805f6388b8b3380be87ca589fea8179c8893f
Vishwas <vishwas.g5@gmail.com>
2023-10-21 07:07:13 +0530

    Add delay

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,8 @@
-export default function Blog() {
-  return <h1>My blog</h1>;
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("all blog posts");
+    }, 2000);
+  });
+  return <h1>My blog post count</h1>;
 }
