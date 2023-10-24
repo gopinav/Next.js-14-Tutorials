8c5a4193f1dc2f64400c74ff547c2d3c1f0603a9
Vishwas <vishwas.g5@gmail.com>
2023-10-24 14:17:35 +0530

    Error handling

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import { notFound } from "next/navigation";
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 
 export default function ProductDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  // Throw error 50% of the time
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
