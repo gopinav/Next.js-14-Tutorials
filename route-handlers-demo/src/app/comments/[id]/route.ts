48a2214da5e7dbd4cd9217a69c5f2d83ac9372a2
Vishwas <vishwas.g5@gmail.com>
2024-01-01 21:02:27 +0530

    PATCH request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -9,3 +9,17 @@ export async function GET(
   );
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
