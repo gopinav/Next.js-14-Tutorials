e122163e2a0e4ab65f9d687382202b95f2554039
Vishwas <vishwas.g5@gmail.com>
2024-01-01 21:01:55 +0530

    POST Request

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -3,3 +3,11 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const id = comments.length + 1;
+  const newComment = { id, text: comment.text };
+  comments.push(newComment);
+  return Response.json(comment);
+}
