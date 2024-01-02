c35785bc89e2d80ca0d96bf0e6b2a29327bf82da
Vishwas <vishwas.g5@gmail.com>
2024-01-02 18:16:54 +0530

    Update POST handler

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -6,8 +6,10 @@ export async function GET() {
 
 export async function POST(request: Request) {
   const comment = await request.json();
-  const id = comments.length + 1;
-  const newComment = { id, text: comment.text };
+  const newComment = { id: comments.length + 1, text: comment.text };
   comments.push(newComment);
-  return Response.json(comment);
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
 }
