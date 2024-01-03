f728f06adad8feb9aa6543b4376239d927bb24ab
Vishwas <vishwas.g5@gmail.com>
2024-01-03 18:03:59 +0530

    Redirection

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,9 +1,13 @@
+import { redirect } from "next/navigation";
 import { comments } from "../data";
 
 export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
+  if (parseInt(params.id) > comments.length) {
+    redirect("/comments");
+  }
   const comment = comments.find(
     (comment) => comment.id === parseInt(params.id)
   );
