17e93bf4514e61e375ece29f81b02ab1f67dd6f3
Vishwas <vishwas.g5@gmail.com>
2024-02-24 16:55:14 +0530

    Dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
+import { cookies } from "next/headers";
+
 export default function AboutPage() {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
   return (
     <>
       <h1>About page {new Date().toLocaleTimeString()}</h1>
