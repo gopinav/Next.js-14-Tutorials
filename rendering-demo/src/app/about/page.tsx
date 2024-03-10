83d15bc8d5a7d5ad8e2449323603259a09265948
Vishwas <vishwas.g5@gmail.com>
2024-03-10 11:43:08 +0530

    Server-only code

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,12 +1,15 @@
 import { cookies } from "next/headers";
+import { serverSideFunction } from "@/utils/server-utils";
 
 export default function AboutPage() {
   const cookieStore = cookies();
   const theme = cookieStore.get("theme");
   console.log(theme);
+  const result = serverSideFunction();
   return (
     <>
       <h1>About page {new Date().toLocaleTimeString()}</h1>
+      <p>{result}</p>
     </>
   );
 }
---
+++ rendering-demo/src/utils/server-utils.ts
@@ -0,0 +1,11 @@
+import "server-only";
+
+export const serverSideFunction = () => {
+  console.log(
+    `use multiple libraries,
+     use environment variables,
+     interact with a database,
+     process confidential information`
+  );
+  return "result";
+};
