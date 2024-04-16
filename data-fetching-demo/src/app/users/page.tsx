dd4d04231605a01a0aabd46106e08a7e0d6f4c24
Vishwas <vishwas.g5@gmail.com>
2024-04-16 21:11:11 +0530

    Reorder

--- data-fetching-demo/src/app/users/page.tsx
+++ data-fetching-demo/src/app/users/page.tsx
@@ -7,8 +7,8 @@ type User = {
 };
 
 export default async function UsersPage() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
   await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await response.json();
   return (
     <div className="grid grid-cols-2 gap-2 p-4">
