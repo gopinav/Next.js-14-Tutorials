967382fac63d89f77552d2e8a4cc950e7057e1e0
Vishwas <vishwas.g5@gmail.com>
2024-04-19 21:49:17 +0530

    Add custom json server

---
+++ data-fetching-demo/server.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  console.log("Request received");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+server.use(router);
+server.listen(3002, () => {
+  console.log("JSON Server is running");
+});
