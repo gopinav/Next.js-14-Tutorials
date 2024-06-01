cfaf148744def9ae641393ab6934cf8cad0cd763
sabeer bikba <59386700+sabeerbikba@users.noreply.github.com>
2024-06-01 17:15:25 +0530

    Update page.tsx: Handling Non-Existent Images
    This update improves the PhotoPage component by adding error handling for scenarios where the specified image ID does not exist in the wondersImages array. If an image is not found, a message is displayed instead of the image. This ensures a better user experience by providing feedback rather than leaving the page blank or breaking.

--- routing-demo/src/app/photo-feed/[id]/page.tsx
+++ routing-demo/src/app/photo-feed/[id]/page.tsx
@@ -6,24 +6,29 @@ export default function PhotoPage({
 }: {
   params: { id: string };
 }) {
-  const photo: WonderImage = wondersImages.find((p) => p.id === id)!;
-  return (
-    <div className="container mx-auto my-10">
-      <div className="w-1/2 mx-auto">
-        <div>
-          <h1 className="text-center text-3xl font-bold my-4">{photo.name}</h1>
-        </div>
-        <Image
-          alt={photo.name}
-          src={photo.src}
-          className="w-full object-cover aspect-square "
-        />
+    try {
+        const photo: WonderImage = wondersImages.find((p) => p.id === id)!;
+        return (
+            <div className="container mx-auto my-10">
+                <div className="w-1/2 mx-auto">
+                    <div>
+                        <h1 className="text-center text-3xl font-bold my-4">{photo.name}</h1>
+                    </div>
+                    <Image
+                        alt={photo.name}
+                        src={photo.src}
+                        className="w-full object-cover aspect-square "
+                    />
 
-        <div className="bg-white py-4">
-          <h3>{photo.photographer}</h3>
-          <h3>{photo.location}</h3>
-        </div>
-      </div>
-    </div>
-  );
+                    <div className="bg-white py-4">
+                        <h3>{photo.id}</h3>
+                        <h3>{photo.site}</h3>
+                    </div>
+                </div>
+            </div>
+        );
+    } catch {
+        console.log('if');
+        return <h1>no image to show </h1>
+    }
 }
